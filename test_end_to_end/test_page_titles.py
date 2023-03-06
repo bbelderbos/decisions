e0cf2bd9d04a48c143a553b2852e99617fcd2d51
Robin Beer <r.beer@outlook.de>
2023-03-06 20:39:09 +0100

    change test API_URL

--- test_end_to_end/test_page_titles.py
+++ test_end_to_end/test_page_titles.py
@@ -4,13 +4,19 @@
 import re
 
 import pytest
+from decouple import config
 from playwright.sync_api import Page, expect
+
+DEBUG = config("DEBUG", default=False, cast=bool)
+
+API_URL = (
+    "http://localhost:8000/decisions/" if DEBUG else "http://myapi:8000/decisions/"
+)
 
 
 def test_homepage(page: Page):
-    page.goto("http://localhost:8080")
-    
-    
+    page.goto(API_URL)
+
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("overview"))
 
@@ -18,28 +24,29 @@ def test_homepage(page: Page):
     new = page.get_by_role("link", name="new")
     update = page.get_by_role("link", name="update")
 
-    new.click() # click on the page "new"
+    new.click()  # click on the page "new"
 
     expect(page).to_have_title(re.compile("new"))
 
     page.screenshot(path="screenshot.png", full_page=True)
 
+
 @pytest.mark.order(after="test_homepage")
 def test_archive_decisions_exist(page: Page):
-    page.goto("http://localhost:8080")
+    page.goto(API_URL)
 
     archive = page.get_by_role("link", name="archived")
 
     archive.click()
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("archived"))
-    
+
     # TODO: add a check that the page contains the archived decisions
 
 
 def test_page_new(page: Page):
-    page.goto("http://localhost:8080")
-    
+    page.goto(API_URL)
+
     new = page.get_by_role("link", name="new")
 
     new.click()
@@ -55,10 +62,10 @@ def test_page_new(page: Page):
 
 @pytest.mark.order(after="test_page_new")
 def test_archive_decision_action(page: Page):
-    page.goto("http://localhost:8080")
+    page.goto(API_URL)
 
     # Expect a title "to contain" a substring.
-    
+
     page.get_by_role("combobox", name="Select a decision").fill("Go to sleep?")
 
     page.get_by_role("button", name="Archive").click()
